b15e58397f0de6f3162dbd80d205e5a0bf5d0879
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-04-29 16:40:25 +0530

    Initial Commit

---
+++ app/src/main/java/tech/solarc/MainActivity.java
@@ -0,0 +1,13 @@
+gigit package tech.solarc;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
