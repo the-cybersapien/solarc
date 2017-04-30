642d51a1d282457c1620f1968abc1dcb4cb6474e
nikhil <nikhil97.nb@gmail.com>
2017-04-30 08:12:38 +0530

    Add: Default Data to Appliance Table

--- app/src/main/java/tech/solarc/data/ApplianceDbHelper.java
+++ app/src/main/java/tech/solarc/data/ApplianceDbHelper.java
@@ -19,7 +19,7 @@ public class ApplianceDbHelper extends SQLiteOpenHelper {
             AppliaceEntry._ID + " INTEGER PRIMARY KEY AUTOINCREMENT, " +
             AppliaceEntry.COLUMN_NAME_TITLE + " TEXT NOT NULL, " +
             AppliaceEntry.COLUMN_NAME_PPD + " REAL, "+
-            AppliaceEntry.COLUMN_NAME_QUANTITY + " INTEGER, " +
+            AppliaceEntry.COLUMN_NAME_QUANTITY + " INTEGER DEFAULT 0," +
             AppliaceEntry.COLUMN_NAME_PREF + " INTEGER NOT NULL" +
             ");";
 
@@ -30,6 +30,54 @@ public class ApplianceDbHelper extends SQLiteOpenHelper {
     @Override
     public void onCreate(SQLiteDatabase db) {
         db.execSQL(CREATE_SQL_ENTRIES);
+
+        // for adding Ceiling fan
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Ceiling Fan, " + 1.50 + ", " + 1+");");
+
+        // for CFL
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "CFL, " + 0.21 + ", " + 1+");");
+
+        //for TV
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Television, " + 0.55 + ", " + 3+");");
+
+        //for laptop
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Laptop, " + 0.36 + ", " + 2+");");
+
+        //for Refrigerator
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Refrigerator, " + 4.32 + ", " + 5+");");
+
+        // for cellPhone
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Cell Phone, " + 0.01 + ", " + 2+");");
+
+        // for Washing  Machine
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Washing Machine, " + 0.13 + ", " + 4+");");
+
+        //for Iron
+        db.execSQL("INSERT INTO TABLE " +
+                AppliaceEntry.TABLE_NAME + " (" + AppliaceEntry.COLUMN_NAME_TITLE + ", " +
+                AppliaceEntry.COLUMN_NAME_PPD + ", " + AppliaceEntry.COLUMN_NAME_PREF  +")" +
+                " VALUES (" + "Iron, " + 0.28 + ", " + 4+");");
     }
 
     @Override
