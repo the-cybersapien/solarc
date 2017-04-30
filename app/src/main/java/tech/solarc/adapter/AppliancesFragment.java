aea77bf0f229f2cbdf4bbd73c9f189112864a279
sagarchoudhary96 <sagar.choudhary96@gmail.com>
2017-04-30 08:44:09 +0530

    Add: Appliance Fragment UI

---
+++ app/src/main/java/tech/solarc/adapter/AppliancesFragment.java
@@ -0,0 +1,39 @@
+package tech.solarc.adapter;
+
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.shawnlin.numberpicker.NumberPicker;
+
+import tech.solarc.R;
+
+
+/**
+ * A simple {@link Fragment} subclass.
+ */
+public class AppliancesFragment extends Fragment {
+
+
+    public AppliancesFragment() {
+        // Required empty public constructor
+    }
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        View rootView = inflater.inflate(R.layout.fragment_appliances, container, false);
+
+        NumberPicker picker = (NumberPicker) rootView.findViewById(R.id.number_picker);
+        int value = picker.getValue();
+
+
+        return rootView;
+    }
+
+}
